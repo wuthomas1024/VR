266a8cb68895bd7f68010ec7156b00d46ca00b96
wuthomas1024 <160296137+wuthomas1024@users.noreply.github.com>
2024-05-02 11:42:05 +0800

    1

---
+++ main/data - 複製.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "旗桿處",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -1.3592263338785422,
+        "pitch": 0.12080959491239085,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "首頁",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
